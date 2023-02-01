59c47dc6e55244946e8d67177aa3403987b3fb7b
Pierre van Niekerk <PvanNiekerk87@gmail.com>
2023-02-01 06:48:41 +0200

    Added Mediator pattern

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,32 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Application.Activities;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseAPIController
     {
-        public DataContext Context { get; }
-        public ActivitiesController(DataContext context)
-        {
-            this.Context = context;
-        }
-
-        [HttpGet] //api/activites
+        [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await this.Context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query());
         }
 
-        [HttpGet("{id}")] //api/acitivities/
+        [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await this.Context.Activities.FindAsync(id);
+            return await Mediator.Send(new Details.Query { Id = id });
         }
     }
 }
